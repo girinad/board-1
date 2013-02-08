1697360fb140a1c0550dc8281a6da1a8d4e1f356
girinad <girinad@gmail.com>
2013-02-08 17:40:47 +0400

    Add tests for mailer methods

--- spec/mailers/board_mailer_spec.rb
+++ spec/mailers/board_mailer_spec.rb
@@ -1,5 +1,37 @@
+# encoding: UTF-8
+
 require "spec_helper"
 
 describe BoardMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#user_banned_email' do
+
+    it 'Отсылает письмо о том что пользователь заблокирован' do
+      @user = FactoryGirl.create :user
+      BoardMailer.user_banned_email(@user).deliver
+
+      message = ActionMailer::Base.deliveries.last
+
+      message.to.should include @user.email
+      message.subject.should have_text I18n.t(:user_banned_title)
+      message.body.should have_text I18n.t(:user_banned_title)
+      message.body.should have_text I18n.t(:user_banned_message)
+    end
+
+  end
+
+  describe '#photo_banned_email' do
+
+    it 'Отсылает письмо о том что картинка заблокирована' do
+      @user = FactoryGirl.create :user
+      BoardMailer.photo_banned_email(@user).deliver
+
+      message = ActionMailer::Base.deliveries.last
+
+      message.to.should include @user.email
+      message.subject.should have_text I18n.t(:photo_banned_title)
+      message.body.should have_text I18n.t(:photo_banned_title)
+      message.body.should have_text I18n.t(:photo_banned_message)
+    end
+
+  end
 end
